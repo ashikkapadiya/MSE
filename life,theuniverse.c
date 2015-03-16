1fb3806d188dbca79da5c608c4bbd174ea865426
ashikkapadiya <ashikkapadiya@gmail.com>
2015-03-16 11:21:35 +0000

    jxnjsacnsa

---
+++ life,theuniverse.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int number,i;
+    scanf("%d\n",&number);
+    for(i=0;;i++)
+    {
+        if(number==42)
+            break;
+        else
+            printf("%d\n",number);
+            scanf("%d\n",&number);
+    }
+}
